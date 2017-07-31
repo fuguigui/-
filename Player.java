97d11f9539f903a8c3caa552c27c71fc4fd19607
Fufu <fgr079@126.com>
2017-07-31 23:17:26 +0800

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,14 +1,22 @@
 
-public class Player {
-	private boolean isBlack;//记录player的身份：是不是黑子
+abstract class Player {
+	protected boolean isBlack;
 	
 	public Player(boolean isblack){
 		isBlack = isblack;
 	}
 	
-	public int nextPoint(State curr){//根据当前的状态，决定最终的下棋位置
+	public int nextPoint(State curr){
 		
 		return 0;
 	}
 
+}
+
+class Human extends Player{
+
+	public Human(boolean isblack) {
+		super(isblack);
+	}
+	
 }
